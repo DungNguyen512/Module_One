c36d1dcceefff7f9c407379f3037c9fd394d635b
DungNguyen512 <dungipn512@gmail.com>
2020-05-09 10:48:39 +0700

    UI facebook

--- app/src/main/java/com/example/module_one/Main3Activity.java
+++ app/src/main/java/com/example/module_one/Main3Activity.java
@@ -1,14 +1,38 @@
 package com.example.module_one;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener {
 
-public class Main3Activity extends AppCompatActivity {
+    EditText txtEmail, txtPassword;
+    Button btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        txtEmail = findViewById(R.id.edtEmail);
+        txtPassword = findViewById(R.id.edtPassword);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(Main3Activity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnLogIn) {
+            String email = txtEmail.getText().toString().trim();
+            if (!TextUtils.isEmpty(email)) {
+                Toast.makeText(Main3Activity.this, "Wellcome" + " " + email, Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
